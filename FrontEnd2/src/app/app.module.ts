df3713c2662d6bc3d5dff2906966ed654a0a4418
Jessie Brenes <jessiejosuebrenesruiz@gmail.com>
2022-06-17 22:23:29 -0600

    creacion de componentes login and department
    tambien añadi sus servicios y algunas carpetas

--- FrontEnd2/src/app/app.module.ts
+++ FrontEnd2/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/template/footer/footer.component';
+import { DepartmentComponent } from './shared/components/department/department.component';
+import { NavbarComponent } from './shared/template/navbar/navbar.component';
+import { LoginComponent } from './shared/components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    DepartmentComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
