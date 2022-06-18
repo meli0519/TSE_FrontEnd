8ed14f07f4a6e095fef626569173118188a81d95
Meli <MMCalexandra@gmail.com>
2022-06-18 01:08:45 -0600

    modal

--- FrontEnd2/src/app/shared/modules/main/main-routing.module.ts
+++ FrontEnd2/src/app/shared/modules/main/main-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentComponent } from '../../components/department/department.component';
+import { UserComponent } from '../../components/user/user.component';
+
 const routes: Routes = [
   {
     path: "",
     children: [
-      {path: 'department', component: DepartmentComponent}
+      {path: 'department', component: DepartmentComponent},
+      {path: 'user', component:  UserComponent}
     ]
   }
 ];
