47799d087e4e6de44965acaac02765b4c7932939
Jessie Brenes <jessiejosuebrenesruiz@gmail.com>
2022-06-17 22:41:38 -0600

    Login

--- FrontEnd2/src/app/app.module.ts
+++ FrontEnd2/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/template/footer/footer.component';
 import { DepartmentComponent } from './shared/components/department/department.component';
 import { NavbarComponent } from './shared/template/navbar/navbar.component';
 import { LoginComponent } from './shared/components/login/login.component';
-
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +18,10 @@ import { LoginComponent } from './shared/components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
